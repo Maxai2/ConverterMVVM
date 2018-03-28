63d1aa51eda47b300ed96da382fafb20ae915bc3
Хамелеон <maxali946@gmail.com>
2018-03-28 18:04:25 +0400

    50%

--- ConverterMVVM/ConverterMVVM/View/MainWindow.xaml.cs
+++ ConverterMVVM/ConverterMVVM/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConverterMVVM.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace ConverterMVVM.View
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new ConvertClass();
         }
     }
 }
--- ConverterMVVM/ConverterMVVM/ViewModel/ConvertClass.cs
+++ ConverterMVVM/ConverterMVVM/ViewModel/ConvertClass.cs
@@ -1,12 +1,74 @@
-﻿using System;
+﻿using ConverterMVVM.Common;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ConverterMVVM.ViewModel
 {
-    class ConvertClass
+    //public class Currence
+    //{
+    //    public string Name { get; set; }
+    //    public double Value { get; set; }
+
+    //    public Currence(string Name, double Value)
+    //    {
+    //        this.Name = Name;
+    //        this.Value = Value;
+    //    }
+    //}
+
+    public class ConvertClass
     {
+        public ObservableCollection<string> currencyItem { get; set; }
+
+        public ConvertClass()
+        {
+            currencyItem = new ObservableCollection<string>()
+            {
+                "USD",
+                "AZN",
+                "RUB",
+                "EUR"
+            };
+        }
+
+
+        private void tbValue_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
+
+        void Convert(string FCur, string TCur, double value)
+        {
+            
+        }
+
+        private IComparable convertCommand;
+
+        //public IComparable ConvertCommand
+        
+            //get
+            //{
+            //    if (this.convertCommand is null)
+            //    {
+            //        this.convertCommand = new RelayCommand(
+            //            (param) => 
+            //            {
+                            
+            //            }
+            //            )
+            //    }
+            //}
+            
+        
+
+
     }
 }
